e08d672f68c2827c5e373090807d0319d38b638e
Denis Kondratyev <kondratyev_dv@mail.ru>
2023-09-03 13:52:43 +0300

    Реализовать кнопку «Написать в поддержку»

--- app/src/main/java/com/practium/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/practium/playlistmaker/SettingsActivity.kt
@@ -15,6 +15,7 @@ class SettingsActivity : AppCompatActivity() {
         val backToMainMenu = findViewById<TextView>(R.id.activity_setting_header)
         val shareAppButton = findViewById<Button>(R.id.activity_settings_share_app_button)
 
+        val sendEMailToSupportButton = findViewById<Button>(R.id.activity_settings_mail_to_support_button)
 
         backToMainMenu.setOnClickListener {
             finish()
@@ -25,7 +26,21 @@ class SettingsActivity : AppCompatActivity() {
             val shareAppDisplay = Intent(Intent.ACTION_SEND)
             shareAppDisplay.data = Uri.parse("https:")
             shareAppDisplay.putExtra(Intent.EXTRA_TEXT, "https://practicum.yandex.ru/android-developer/?from=catalog")
+            shareAppDisplay.type = "text/plain"
             startActivity(shareAppDisplay)
+        }
+
+        sendEMailToSupportButton.setOnClickListener{
+            val mailToAddress = arrayOf("kondratyev_dv@mail.ru")
+            val mailSubject = "Сообщение разработчикам и разработчицам приложения Playlist Maker"
+            val mailText = "Спасибо разработчикам и разработчицам за крутое приложение!"
+
+            val sendEmailToSupportIntent = Intent(Intent.ACTION_SENDTO)
+            sendEmailToSupportIntent.data = Uri.parse("mailto:")
+            sendEmailToSupportIntent.putExtra(Intent.EXTRA_EMAIL, mailToAddress)
+            sendEmailToSupportIntent.putExtra(Intent.EXTRA_TEXT, mailText)
+            sendEmailToSupportIntent.putExtra(Intent.EXTRA_SUBJECT, mailSubject)
+            startActivity(sendEmailToSupportIntent)
         }
 
 

83eccfa6f51b13cd16ef37e1007ed2a3404c889e
Denis Kondratyev <kondratyev_dv@mail.ru>
2023-09-03 13:37:37 +0300

    Реализовать кнопку «Поделиться приложением»

--- app/src/main/java/com/practium/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/practium/playlistmaker/SettingsActivity.kt
@@ -1,7 +1,10 @@
 package com.practium.playlistmaker
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 
 class SettingsActivity : AppCompatActivity() {
@@ -10,8 +13,21 @@ class SettingsActivity : AppCompatActivity() {
         setContentView(R.layout.activity_settings)
 
         val backToMainMenu = findViewById<TextView>(R.id.activity_setting_header)
+        val shareAppButton = findViewById<Button>(R.id.activity_settings_share_app_button)
+
+
         backToMainMenu.setOnClickListener {
             finish()
         }
+
+        shareAppButton.setOnClickListener {
+
+            val shareAppDisplay = Intent(Intent.ACTION_SEND)
+            shareAppDisplay.data = Uri.parse("https:")
+            shareAppDisplay.putExtra(Intent.EXTRA_TEXT, "https://practicum.yandex.ru/android-developer/?from=catalog")
+            startActivity(shareAppDisplay)
+        }
+
+
     }
 }

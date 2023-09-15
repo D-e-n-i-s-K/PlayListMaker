06321b780ec549b6af381eca65a477742113e000
Denis Kondratyev <kondratyev_dv@mail.ru>
2023-09-15 20:46:28 +0300

    Выполнить задачу 10 спринта

--- app/src/main/java/com/practium/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/practium/playlistmaker/SearchActivity.kt
@@ -4,12 +4,20 @@ import android.content.Context
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.util.TypedValue
+import android.view.LayoutInflater
+import android.view.RoundedCorner
 import android.view.View
+import android.view.ViewGroup
 import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 
 class SearchActivity : AppCompatActivity() {
 
@@ -20,9 +28,56 @@ class SearchActivity : AppCompatActivity() {
     private lateinit var searchText: String
     private var searchEditText: EditText? = null
 
+    class TrackHolder(private val view: View) : RecyclerView.ViewHolder(view){
+        private val trackName : TextView = view.findViewById(R.id.track_name)
+        private val artistName : TextView = view.findViewById(R.id.artist_name)
+        private val trackTime : TextView = view.findViewById(R.id.track_time)
+        private val artwork : ImageView = view.findViewById(R.id.artwork)
+
+        fun bind(model: Track){
+            trackName.text = model.trackName
+            artistName.text = model.artistName
+            trackTime.text = model.trackTime
+            Glide.with(view)
+                .load(model.artworkUrl100)
+                .placeholder(R.drawable.placeholder)
+                .centerCrop()
+                .transform(RoundedCorners(dpToPix(2F, view.context)))
+                .into(artwork)
+
+        }
+
+        private fun dpToPix(dp: Float, context: Context): Int {
+            return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, context.resources.displayMetrics).toInt()
+        }
+    }
+
+
+
+    class TrackAdapter(private val trackList: ArrayList<Track>) : RecyclerView.Adapter<TrackHolder>() {
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TrackHolder {
+            val view = LayoutInflater.from(parent.context).inflate(R.layout.track_card, parent, false)
+            return TrackHolder(view)
+        }
+
+        override fun onBindViewHolder(holder: TrackHolder, position: Int) {
+            holder.bind(trackList[position])
+        }
+
+        override fun getItemCount(): Int {
+            return trackList.size
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+
+        val trackList = getTestTrackList()
+
+        val trackListRecyclerView = findViewById<RecyclerView>(R.id.trackList)
+        trackListRecyclerView.layoutManager = LinearLayoutManager(this)
+        trackListRecyclerView.adapter = TrackAdapter(trackList)
 
         val cancelButton = findViewById<ImageView>(R.id.cancelButton)
         cancelButton.visibility = View.GONE
@@ -61,6 +116,7 @@ class SearchActivity : AppCompatActivity() {
         }
     }
 
+
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
         searchText = searchEditText?.text.toString()
@@ -81,6 +137,42 @@ class SearchActivity : AppCompatActivity() {
         } else {
             View.VISIBLE
         }
+    }
 
+    private fun getTestTrackList(): ArrayList<Track> {
+
+        return arrayListOf(
+            Track(
+                getString(R.string.test_trackName_1),
+                getString(R.string.test_artistName_1),
+                getString(R.string.test_trackTime_1),
+                getString(R.string.test_artworkUrl100_1),
+            ),
+            Track(
+                getString(R.string.test_trackName_2),
+                getString(R.string.test_artistName_2),
+                getString(R.string.test_trackTime_2),
+                getString(R.string.test_artworkUrl100_2),
+            ),
+            Track(
+                getString(R.string.test_trackName_3),
+                getString(R.string.test_artistName_3),
+                getString(R.string.test_trackTime_3),
+                getString(R.string.test_artworkUrl100_3),
+            ),
+            Track(
+                getString(R.string.test_trackName_4),
+                getString(R.string.test_artistName_4),
+                getString(R.string.test_trackTime_4),
+                getString(R.string.test_artworkUrl100_4),
+            ),
+            Track(
+                getString(R.string.test_trackName_5),
+                getString(R.string.test_artistName_5),
+                getString(R.string.test_trackTime_5),
+                getString(R.string.test_artworkUrl100_5),
+            ),
+
+        )
     }
 }
